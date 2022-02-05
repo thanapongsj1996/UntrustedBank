811a38978e97d5a18a77be1db1ef1533f2a72810
thanapongsj <thanapongsj1996@gmail.com>
2022-02-05 23:10:27 +0700

    remove lp token deployment

--- services/deployer.go
+++ services/deployer.go
@@ -5,7 +5,6 @@ import (
 
 	bank "github.com/thanapongsj1996/untrustedbank/bindings/bank"
 	jerryToken "github.com/thanapongsj1996/untrustedbank/bindings/jerry-token"
-	lpToken "github.com/thanapongsj1996/untrustedbank/bindings/lp-token"
 	pool "github.com/thanapongsj1996/untrustedbank/bindings/pool"
 	tomToken "github.com/thanapongsj1996/untrustedbank/bindings/tom-token"
 	"github.com/thanapongsj1996/untrustedbank/consts"
@@ -40,10 +39,6 @@ func (svc *Deployer) Deploy() error {
 	if err != nil {
 		return err
 	}
-	lpTokenAddr, lpTokenTx, _, err := lpToken.DeployLptoken(utils.MySendOpt(client, network), client)
-	if err != nil {
-		return err
-	}
 	bankAddr, bankTx, _, err := bank.DeployBank(utils.MySendOpt(client, network), client, tomTokenAddr)
 	if err != nil {
 		return err
@@ -55,13 +50,11 @@ func (svc *Deployer) Deploy() error {
 
 	fmt.Printf("TOM_TOKEN_TX=%s\n", tomTokenTx.Hash().Hex())
 	fmt.Printf("JERRY_TOKEN_TX=%s\n", jerryTokenTx.Hash().Hex())
-	fmt.Printf("LP_TOKEN_TX=%s\n", lpTokenTx.Hash().Hex())
 	fmt.Printf("BANK_TX=%s\n", bankTx.Hash().Hex())
 	fmt.Printf("POOL_TX=%s\n", poolTx.Hash().Hex())
 	fmt.Printf("-------------------\n")
 	fmt.Printf("TOM_TOKEN_ADDR=%s\n", tomTokenAddr.String())
 	fmt.Printf("JERRY_TOKEN_ADDR=%s\n", jerryTokenAddr.String())
-	fmt.Printf("LP_TOKEN_ADDR=%s\n", lpTokenAddr.String())
 	fmt.Printf("BANK_ADDR=%s\n", bankAddr.String())
 	fmt.Printf("POOL_ADDR=%s\n", poolAddr.String())
 
